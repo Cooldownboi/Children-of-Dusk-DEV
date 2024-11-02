ff21cb4adbe49423ab1c9b7860400637926e5aed
I will explode <supernenno09@gmail.com>
2024-11-02 17:27:29 +0100

    small fix

--- common/defines/01_defines.lua
+++ common/defines/01_defines.lua
@@ -1,2 +1,4 @@
 NDefines.NGame.START_DATE = "2049.12.1.1"
-NDefines.NDiplomacy.END_DATE = "2064.1.1.1"
+NDefines.NGame.END_DATE = "2064.1.1.1"
+NDefines.NDiplomacy.MAX_OPINION_VALUE = 250
+NDefines.NDiplomacy.MIN_OPINION_VALUE = -250
